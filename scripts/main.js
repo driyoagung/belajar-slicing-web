1206844b8e38bca4d0a1820d173c7fa10b0f53b6
driyoagung <dieoganda@gmail.com>
2024-01-29 10:08:39 +0700

    add complletely

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,17 @@
-// Toggle & Responsive Navbar
+// Toggle & Responsive Navigation
 const navSlide = () => {
   const burger = document.querySelector(".burger");
-  const navList = document.querySelector("nav");
+  const navLists = document.querySelector("nav");
 
   burger.addEventListener("click", () => {
-    navList.classList.toggle("nav-active");
+    // Toggle nav list and burger class
+    navLists.classList.toggle("nav-active");
     burger.classList.toggle("toggle-burger");
   });
 };
+
 navSlide();
+
 // Clear form before unload
 window.onbeforeunload = () => {
   for (const form of document.getElementsByTagName("form")) {
